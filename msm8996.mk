523d1558f40111626336488501df70e92ec113ef
hacker1024 <joshleivenzon@outlook.com>
2019-03-25 19:38:25 +1100

    Havoc bringup

--- msm8996.mk
+++ msm8996.mk
@@ -357,8 +357,8 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
